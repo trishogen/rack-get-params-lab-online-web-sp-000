136e484fe51a7acd8bf170de699df5c9f09c32cd
Trish Ogen <patriciaogen@gmail.com>
2019-07-25 00:03:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,6 +16,8 @@ class Application
     elsif req.path.match(/cart/)
       if @@cart == []
         resp.write "Your cart is empty."
+      else
+        @@cart.each do {|item| resp.write "#{item}\n"}  
       end
     else
       resp.write "Path Not Found"
