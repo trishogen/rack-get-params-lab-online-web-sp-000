cd0c5461abda490b3ea476c74169549efcf1629d
Trish Ogen <patriciaogen@gmail.com>
2019-07-25 00:02:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,8 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      display_cart
+      if @@cart == []
+        resp.write "Your cart is empty."
     else
       resp.write "Path Not Found"
     end
